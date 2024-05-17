5abc84d2f37a8e50bc0ae5e258fc80e3a4a2807c
Rabia Öztürk <149370651+rabiiaoztturk@users.noreply.github.com>
2024-05-17 11:40:47 +0300

    Add files via upload

---
+++ ToDo/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ToDo
+{
+    internal class Program
+    {
+        static string ad;
+
+        static void Secenekler()
+        {
+            List<string> secenekler = new List<string>();
+
+            foreach (string secenek in secenekler)
+            {
+                Console.WriteLine(secenek);
+            }
+
+            Console.WriteLine("ToDo Uygulamasına Hoşgeldiniz :) ");
+            Console.WriteLine($"İsminizi giriniz: ");
+            string ad = Console.ReadLine();
+
+            Console.WriteLine($"Hoşgeldin {ad}!");
+
+            Console.WriteLine("1- Yeni Ekle");
+            Console.WriteLine("2- Girilenleri Listele");
+            Console.WriteLine("3- Girilenleri Sil");
+
+
+            Console.WriteLine("Cevabınız : ");
+            int cevap = int.Parse(Console.ReadLine());
+
+            if(cevap == 1)
+            {
+                Console.WriteLine("Eklemek istediğiniz notu giriniz: ");
+                Console.ReadLine();
+                Console.WriteLine("İşleminiz başarıyla gerçekleşti !");
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Secenekler();
+
+        }
+    }
+}
