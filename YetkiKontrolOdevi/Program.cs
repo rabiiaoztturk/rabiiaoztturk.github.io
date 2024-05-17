5afc0d1a5bc72f98bc74b87dd55b8324374a33e6
Rabia Öztürk <149370651+rabiiaoztturk@users.noreply.github.com>
2024-05-17 11:33:50 +0300

    Add files via upload

---
+++ YetkiKontrolOdevi/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace YetkiKontrolOdevi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kullanıcı adınızı giriniz:");
+            string kullaniciAdi = Console.ReadLine();
+
+            Console.WriteLine("Şifrenizi giriniz");
+            string sifre = Console.ReadLine();
+
+            string guvenlik1 = "rabiiaoztturkk";
+            string guvenlik2 = "13579";
+
+            if (kullaniciAdi == guvenlik1 && sifre == guvenlik2) 
+            {
+                Console.WriteLine("Hoşgeldiniz :)");
+            }
+            else
+            {
+                Console.WriteLine("Yanlış kullanıcı adı veya şifre !");
+            }
+
+
+
+        }
+    }
+}
